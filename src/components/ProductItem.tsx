482cdacab4617ac6611c0313061a96515e7e9ebd
AnshuMahto <anshum.ug23.cs@nitp.ac.in>
2024-11-02 11:04:47 +0530

    flatlist

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -14,6 +14,7 @@ const ProductItem = ({ product }: ProductItemProp) => {
       <Image
         source={{ uri: product.image || defaultUri }}
         style={styles.image}
+        resizeMode={"contain"}
       />
       <Text style={styles.name}>{product.name}</Text>
       <Text style={styles.price}>₹ {product.price}</Text>
@@ -28,6 +29,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
+    flex: 1,
+    maxWidth: "50%",
   },
   image: {
     width: "100%",
