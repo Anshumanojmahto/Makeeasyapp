147b1348d33d4774fa8b5e6e23653c326b373afc
AnshuMahto <anshum.ug23.cs@nitp.ac.in>
2024-11-03 17:42:37 +0530

    final3

--- src/app/(admin)/menu/_layout.tsx
+++ src/app/(admin)/menu/_layout.tsx
@@ -5,27 +5,48 @@ import { Pressable } from "react-native";
 
 const MenuLayout = () => {
   return (
-    <Stack
-      screenOptions={{
-        headerRight: () => (
-          <Link href="/" asChild>
-            <Pressable>
-              {({ pressed }) => (
-                <FontAwesome
-                  name="shopping-cart"
-                  size={25}
-                  color={Colors.light.tint}
-                  style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                />
-              )}
-            </Pressable>
-          </Link>
-        ),
-      }}
-    >
+    <Stack screenOptions={{}}>
       <Stack.Screen
         name="index"
-        options={{ title: "Menu", headerTitleAlign: "center" }}
+        options={{
+          title: "Menu",
+          headerTitleAlign: "center",
+          headerRight: () => (
+            <Link href="/" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="plus-square-o"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="[id]"
+        options={{
+          title: "Menu",
+          headerTitleAlign: "center",
+          headerRight: () => (
+            <Link href="/" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="pencil"
+                    size={25}
+                    color={Colors.light.tint}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
       />
     </Stack>
   );
